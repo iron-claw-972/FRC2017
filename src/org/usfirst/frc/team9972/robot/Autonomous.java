951516cc1969c358f6f8017f1eced4e15d99d27a
Andy Sheu <clubcharger@gmail.com>
2017-01-18 20:21:48 -0800

    added autonomous routine structure using enums

--- src/org/usfirst/frc/team9972/robot/Autonomous.java
+++ src/org/usfirst/frc/team9972/robot/Autonomous.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team9972.robot;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
 public class Autonomous {
 	/*
@@ -13,11 +14,23 @@ public class Autonomous {
 	 * Example: if(r.isEnabled())
 	 */
 	
+	static SendableChooser autoChooser = new SendableChooser();
+
+	public static void createChooser() {
+		autoChooser.addDefault("Do Nothing", AutonomousRoutine.DO_NOTHING);
+		autoChooser.addObject("Cross Baseline", AutonomousRoutine.CROSS_BASELINE);
+		SmartDashboard.putData("Autonomous Mode Chooser", autoChooser);
+	}
+	
 	public static void init(Robot r) {
-		
+		updateSmartDashboardAutonomous();
 	}
 	
 	public static void periodic(Robot r) {
-		
+		updateSmartDashboardAutonomous();
+	}
+	
+	public static void updateSmartDashboardAutonomous() {
+		SmartDashboard.putString("Autonomous Routine", autoChooser.getSelected().toString());
 	}
 }
