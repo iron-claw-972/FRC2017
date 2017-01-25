ea6c87e652a83a69cb8c00176ecdd15ff125743d
Andy Sheu <AndySheu@users.noreply.github.com>
2017-01-25 08:06:16 -0800

    Add constant + fix format

--- src/org/usfirst/frc/team9972/robot/Constants.java
+++ src/org/usfirst/frc/team9972/robot/Constants.java
@@ -10,20 +10,23 @@ public class Constants {
 	 * public static final int FRONT_LEFT_DRIVE_MOTOR_CAN_ID = 1;
 	 */
 	
-	// Motor ID
+	// Motor IDs
 	public static final int FRONT_LEFT_DRIVE_MOTOR_PWM_PORT = 0;
 	public static final int FRONT_RIGHT_DRIVE_MOTOR_PWM_PORT = 1;
 	public static final int BACK_LEFT_DRIVE_MOTOR_PWM_PORT = 2;
 	public static final int BACK_RIGHT_DRIVE_MOTOR_PWM_PORT = 3;
 	
-	//Joystick ID
+	// Joystick IDs
 	public static final int LEFT_JOYSTICK_INPUT_PORT = 0;
 	public static final int RIGHT_JOYSTICK_INPUT_PORT = 1;
 	public static final int OPERATOR_JOYSTICK_INPUT_PORT = 2;
 	
-	//Encoders ID
+	// DIO Ports
 	public static final int LEFT_DRIVE_ENCODER_PORT_A = 0;
 	public static final int LEFT_DRIVE_ENCODER_PORT_B = 1;
 	public static final int RIGHT_DRIVE_ENCODER_PORT_A = 2;
 	public static final int RIGHT_DRIVE_ENCODER_PORT_B = 3;
+	
+	// Left Joystick
+	public static final int INVERSE_DRIVE_TOGGLE_BUTTON = 1;
 }
