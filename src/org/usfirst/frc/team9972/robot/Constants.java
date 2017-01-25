454293d203e2d5ddf15980e18cc2e3061a3c476f
Nikita U <nikita.usenko@gmail.com>
2017-01-24 16:08:37 -0800

    Deleted robot.java because it sucks

--- src/org/usfirst/frc/team9972/robot/Constants.java
+++ src/org/usfirst/frc/team9972/robot/Constants.java
@@ -26,4 +26,6 @@ public class Constants {
 	public static final int LEFT_DRIVE_ENCODER_PORT_B = 1;
 	public static final int RIGHT_DRIVE_ENCODER_PORT_A = 2;
 	public static final int RIGHT_DRIVE_ENCODER_PORT_B = 3;
+	
+	//public static final int INVERT_DRIVE_BUTTON = 1;
 }
