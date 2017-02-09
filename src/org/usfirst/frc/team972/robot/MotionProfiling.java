89911bd3c1e4a1bd80f3b312ad609f8bb65d66a5
Andy Sheu <clubcharger@gmail.com>
2017-02-09 14:24:47 -0800

    Fixed my idiocy

--- src/org/usfirst/frc/team972/robot/MotionProfiling.java
+++ src/org/usfirst/frc/team972/robot/MotionProfiling.java
@@ -33,54 +33,59 @@ public class MotionProfiling {
 		RightModel.update(rightAccel, dT, frontRightEncoderValue * Constants.ROBOT_DRIVE_WHEEL_CIRCUMFERENCE / Constants.ENCODER_CLICKS_PER_ROTATION,
 				backRightEncoderValue * Constants.ROBOT_DRIVE_WHEEL_CIRCUMFERENCE / Constants.ENCODER_CLICKS_PER_ROTATION);
 		SystemModel.update(gyro, dT);
-		// @fotmatter:on
+		// @formatter:on
 	}
-	
+
 	/**
 	 * Initialize at start of match
 	 * 
-	 * @param x_init	Initial x position in meters
-	 * @param y_init	Initial y position in meters
+	 * @param x_init
+	 *            Initial x position in meters
+	 * @param y_init
+	 *            Initial y position in meters
 	 */
 	public static void init(double x_init, double y_init) {
 		LeftModel.setState(0.0, 0.0, 0.0);
 		RightModel.setState(0.0, 0.0, 0.0);
 		SystemModel.setState(x_init, y_init, 0.0, 0.0);
 	}
-	
+
 	/**
 	 * Resets the robot position when at a fixed location (i.e. the gear lift)
 	 * 
-	 * @param x_pos		New x position in meters
-	 * @param y_pos		New y position in meters
-	 * @param theta		New angle in degrees
+	 * @param x_pos
+	 *            New x position in meters
+	 * @param y_pos
+	 *            New y position in meters
+	 * @param theta
+	 *            New angle in degrees
 	 */
 	public static void reset(double x_pos, double y_pos, double theta) {
 		double distance = 0.5 * ((Math.PI * Constants.ROBOT_WIDTH) / 360) * theta;
-		//this makes sure that our system model doesn't mess up if theta isn't zero
+		// this makes sure that our system model doesn't mess up if theta isn't zero
 		LeftModel.setState(distance, 0.0, 0.0);
 		RightModel.setState(-distance, 0.0, 0.0);
 		SystemModel.setState(x_pos, y_pos, 0.0, theta);
 	}
-	
+
 	public static void updateSmartDashboard() {
 		SmartDashboard.putNumber("X Position", getX());
 		SmartDashboard.putNumber("Y Position", getY());
 		SmartDashboard.putNumber("Angle", getTheta());
 	}
-	
+
 	public static double getX() {
 		return SystemModel.x_k;
 	}
-	
+
 	public static double getY() {
 		return SystemModel.y_k;
 	}
-	
+
 	public static double getV() {
 		return SystemModel.v_k;
 	}
-	
+
 	public static double getTheta() {
 		return SystemModel.theta_k;
 	}
