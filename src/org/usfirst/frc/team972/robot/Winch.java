d04911ae82774134ff075d3dbcfda1f06242edf7
Andy Sheu <clubcharger@gmail.com>
2017-02-09 10:04:18 -0800

    Winch javadocs

--- src/org/usfirst/frc/team972/robot/Winch.java
+++ src/org/usfirst/frc/team972/robot/Winch.java
@@ -7,10 +7,16 @@ public class Winch {
 	static boolean winchMotorOn = false;
 	static boolean winchButtonPressedLastTime = false;
 	
+	/**
+	 * Winch initiation sequence.
+	 */
 	public static void init() {
 		Robot.winchMotor.enableBrakeMode(true);
 	}
 	
+	/**
+	 * Manages Winch using joystick toggle input.
+	 */
 	public static void manage() {
 		boolean winchMotorButton = Robot.operatorJoystick.getRawButton(Constants.WINCH_MOTOR_TOGGLE_BUTTON);
 		if (winchMotorButton && !winchButtonPressedLastTime) {
@@ -21,16 +27,25 @@ public class Winch {
 		winchButtonPressedLastTime = winchMotorButton;
 	}
 
+	/**
+	 * Starts Winch.
+	 */
 	public static void start() {
 		Robot.winchMotor.set(Constants.WINCH_MOTOR_SPEED);
 		winchMotorOn = true;
 	}
 
+	/**
+	 * Stops Winch.
+	 */
 	public static void stop() {
 		Robot.winchMotor.set(0);
 		winchMotorOn = false;
 	}
-	
+
+	/**
+	 * Updates SmartDashboard values for Winch.
+	 */
 	public static void updateSmartDashboard() {
 		SmartDashboard.putBoolean("Winch Motor", winchMotorOn);
 	}
