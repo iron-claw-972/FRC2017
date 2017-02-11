4f55768740d0899a0f393b99dfb49f27387c31b5
Ari Schreiman <Ari@Schreiman.net>
2017-02-10 23:45:14 -0800

    test auto code for Drive.autonDrive funcction once we have the drive base

--- src/org/usfirst/frc/team972/robot/Autonomous.java
+++ src/org/usfirst/frc/team972/robot/Autonomous.java
@@ -17,6 +17,7 @@ public class Autonomous {
 	private static double prevTime = 0.0;
 	
 	static SendableChooser autoChooser = new SendableChooser();
+	static AutonomousRoutine selectedAutoRoutine;
 
 	/**
 	 * Create Autonomous chooser for SmartDashboard.
@@ -24,9 +25,16 @@ public class Autonomous {
 	public static void createChooser() {
 		autoChooser.addDefault("Do Nothing", AutonomousRoutine.DO_NOTHING);
 		autoChooser.addObject("Cross Baseline", AutonomousRoutine.CROSS_BASELINE);
+		autoChooser.addObject("Test 0 - stay in place", AutonomousRoutine.TEST_0);
+		autoChooser.addObject("Test 1 - rotate 90 degrees", AutonomousRoutine.TEST_1);
+		autoChooser.addObject("Test 2 - rotate 180 degrees", AutonomousRoutine.TEST_2);
+		autoChooser.addObject("Test 3 - move forward 3 meters", AutonomousRoutine.TEST_3);
+		autoChooser.addObject("Test 4 - move forward 3 meters and turn 90 degrees", AutonomousRoutine.TEST_4);
+		autoChooser.addObject("Test 5 - move to the right 3 meters and end up turned 90 degrees", AutonomousRoutine.TEST_5);
+		autoChooser.addObject("Test 6 - move backwards to x=-1, y=-2", AutonomousRoutine.TEST_6);
 		SmartDashboard.putData("Autonomous Mode Chooser", autoChooser);
 	}
-
+	
 	/**
 	 * Initialization code for autonomous mode. This method for initialization code will be called
 	 * each time the robot enters autonomous mode.
@@ -35,7 +43,36 @@ public class Autonomous {
 	 */
 	public static void init(Robot r) {
 		r.init();
-//		MotionProfiling.init(x_init, y_init); // TODO: figure out what these inits
+		selectedAutoRoutine = (AutonomousRoutine) autoChooser.getSelected();
+		switch (selectedAutoRoutine) {
+			case DO_NOTHING:
+				MotionProfiling.init(0.0, 0.0); //TODO: set all of these for actual competition
+				break;
+			case CROSS_BASELINE:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_0:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_1:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_2:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_3:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_4:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_5:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+			case TEST_6:
+				MotionProfiling.init(0.0, 0.0);
+				break;
+		}
 		updateSmartDashboard();
 	}
 
@@ -52,6 +89,35 @@ public class Autonomous {
 	public static void periodic(Robot r) {
 		double currTime = Time.get();
 		double loopTime = currTime - prevTime;
+		switch (selectedAutoRoutine) {
+			case DO_NOTHING:
+				Drive.stopDrive(); //TODO: set all of these for actual competition
+				break;
+			case CROSS_BASELINE:
+				Drive.tankDrive(0.5, 0.5); //eventually will change to autonDrive
+				break;
+			case TEST_0:
+				Drive.autonDrive(0.0, 0.0, 0.0, loopTime);
+				break;
+			case TEST_1:
+				Drive.autonDrive(0.0, 0.0, 90, loopTime);
+				break;
+			case TEST_2:
+				Drive.autonDrive(0.0, 0.0, 180, loopTime);
+				break;
+			case TEST_3:
+				Drive.autonDrive(0.0, 3.0, 0.0, loopTime);
+				break;
+			case TEST_4:
+				Drive.autonDrive(0.0, 3.0, 90.0, loopTime);
+				break;
+			case TEST_5:
+				Drive.autonDrive(3.0, 0.0, 90.0, loopTime);
+				break;
+			case TEST_6:
+				Drive.autonDrive(-1.0, -2.0, 0.0, loopTime);
+				break;
+		}
 		Drive.updateModel(loopTime);
 		updateSmartDashboard();
 		prevTime = currTime;
@@ -61,7 +127,7 @@ public class Autonomous {
 	 * Updates SmartDashboard values for Autonomous by calling other update functions.
 	 */
 	public static void updateSmartDashboard() {
-		SmartDashboard.putString("Autonomous Routine", autoChooser.getSelected().toString());
+		SmartDashboard.putString("Autonomous Routine", selectedAutoRoutine.toString());
 		Drive.updateSmartDashboard();
 		Shooter.updateSmartDashboard();
 		Winch.updateSmartDashboard();
