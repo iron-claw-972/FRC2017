23bdee0dab579ec506fa6cecd877abc334412a54
Jerry Zhang <jerry.w.zhang@gmail.com>
2017-02-11 17:55:45 -0800

    thread fix

--- src/org/usfirst/frc/team972/robot/CameraStreaming.java
+++ src/org/usfirst/frc/team972/robot/CameraStreaming.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team972.robot;
 
 import edu.wpi.cscore.*;
+import edu.wpi.first.wpilibj.CameraServer;
 
 public class CameraStreaming {
 	private static boolean pressed;
@@ -20,9 +21,13 @@ public class CameraStreaming {
 		usbCamera1 = new UsbCamera("Cam 1", 0);
 		usbCamera2 = new UsbCamera("Cam 2", 1);
 		usbCamera3 = new UsbCamera("Cam 3", 2);
-
+		
+		System.out.println("camera starting");
+		
 		(new  Thread(){
-			cameraSink = CameraStreamingServer.getInstance().startAutomaticCapture(usbCamera1);
+			public void run() {
+				cameraSink = CameraStreamingServer.getInstance().startAutomaticCapture(usbCamera1);
+			}
 		}).start();
 
 	}
@@ -31,14 +36,19 @@ public class CameraStreaming {
 	public static void periodic() {
 		pressed = Robot.leftJoystick.getRawButton(Constants.INVERSE_DRIVE_TOGGLE_BUTTON);
 		if(pressed && !pressedLastTime) {
+			
 				switch (whichCam) {
 					case 1: cameraSink.setSource(usbCamera1);
+							System.out.println("1");
 							break;
 					case 2: cameraSink.setSource(usbCamera2);
+						System.out.println("2");
 						break;
 					case 3: cameraSink.setSource(usbCamera3);
+						System.out.println("3");
 						break;
 				}
+				
 			whichCam++;
 			if(whichCam > NumberOfCameras) whichCam = 1;
 		}
