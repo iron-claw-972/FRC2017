dd230692b4d8bb968ee84c13facb810aaa180b28
Ari Schreiman <AriCodeS@users.noreply.github.com>
2017-03-08 16:57:36 -0800

    Update DriveLoopRedundant.java

--- src/org/usfirst/frc/team972/robot/DriveLoopRedundant.java
+++ src/org/usfirst/frc/team972/robot/DriveLoopRedundant.java
@@ -4,10 +4,12 @@ import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
 
 public class DriveLoopRedundant {
+	double PID_DIVISION_FACTOR = 100000;
+	
 	public static void drive(CANTalon can, boolean inDrive, double value) {
 		if(inDrive) {
 			can.set(value);
-			can.setPID((double)120000/Constants.PID_DIVISION_FACTOR, (double)350/(Constants.PID_DIVISION_FACTOR), (double)10000/Constants.PID_DIVISION_FACTOR);
+			can.setPID((double)120000/PID_DIVISION_FACTOR, (double)350/(PID_DIVISION_FACTOR), (double)10000/PID_DIVISION_FACTOR);
 		} else {
 			can.changeControlMode(TalonControlMode.Voltage);
 			can.enableBrakeMode(false);
