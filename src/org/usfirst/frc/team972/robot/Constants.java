eb6fdd8a28daa69bcab4badbe0bcab209b04d307
Justin Quan <jnicquan@gmail.com>
2017-03-10 15:47:15 -0800

    added pwm ports for led strip

--- src/org/usfirst/frc/team972/robot/Constants.java
+++ src/org/usfirst/frc/team972/robot/Constants.java
@@ -56,12 +56,12 @@ public class Constants {
 	// PWM IDs
 	public static final int LEFT_HOOD_LINEAR_ACTUATOR_PWM_PORT = 0;
 	public static final int RIGHT_HOOD_LINEAR_ACTUATOR_PWM_PORT = 1;
-	public static final int LEFT_RED_LED_PORT = 2;
-	public static final int LEFT_GREEN_LED_PORT = 3;
-	public static final int LEFT_BLUE_LED_PORT = 4;
-	public static final int RIGHT_RED_LED_PORT = 5;
-	public static final int RIGHT_GREEN_LED_PORT = 6;
-	public static final int RIGHT_BLUE_LED_PORT = 7;
+	public static final int LEFT_RED_LED_PWM_PORT = 2;
+	public static final int LEFT_GREEN_LED_PWM_PORT = 3;
+	public static final int LEFT_BLUE_LED_PWM_PORT = 4;
+	public static final int RIGHT_RED_LED_PWM_PORT = 5;
+	public static final int RIGHT_GREEN_LED_PWM_PORT = 6;
+	public static final int RIGHT_BLUE_LED_PWM_PORT = 7;
 	// USB Ports
 	public static final int LEFT_JOYSTICK_INPUT_USB_PORT = 1;
 	public static final int RIGHT_JOYSTICK_INPUT_USB_PORT = 2;
